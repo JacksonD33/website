db918fef85907db1badbb4dc2228a588075da45a
JacksonD33 <jacksondoley@gmail.com>
2024-06-23 20:04:43 +0930

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+});
+app.get('/test', (req, res) => {
+    res.send('This is a test page');
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
